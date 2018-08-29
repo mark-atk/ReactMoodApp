42a93f5599c2f30aeaeb9e4ed1bc9b7b212ff678
Mark Atkinson <markatkinson0@gmail.com>
2018-08-29 17:24:09 +0200

    Added draggables and line drawing.
    Added draggables and line drawing.

---
+++ src/components/wf-designer/designer-panel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class DesignerPanel extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <span>
+                Designer
+            </span>
+        )
+    }
+
+}
+
+export default DesignerPanel;
